860adde4966cc74cbe6cbd169413ed28c55b326e
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-02 16:06:57 +0530

    Read JSON data and create POJO

--- src/main/java/com/shivam/App.java
+++ src/main/java/com/shivam/App.java
@@ -1,5 +1,9 @@
 package com.shivam;
 
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+    	try {
+    		ObjectMapper mapper  = new ObjectMapper();
+    		
+    		Student s = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+    		
+    		System.out.println(s.getFirstName() + " "+s.getLastName()+" active status is : "+s.isActive());
+    		
+    	}catch(Exception e) {
+    		e.printStackTrace();
+    	}
+    	
     }
 }
